fe99ee02ae3b25a54a214704215740b38c23cccb
aditisethia <138678040+aditisethia@users.noreply.github.com>
2024-02-05 17:46:21 +0530

    Update ClassModel.java

--- src/main/java/com/aditi/app/model/ClassModel.java
+++ src/main/java/com/aditi/app/model/ClassModel.java
@@ -2,7 +2,7 @@ package com.aditi.app.model;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -27,7 +27,7 @@ public class ClassModel {
 	private School school;
 	
 	
-	@OneToMany
+	@OneToMany(cascade = CascadeType.ALL)
 	private List<Student> student = new ArrayList<>();
 	
 	private Boolean status = true;
