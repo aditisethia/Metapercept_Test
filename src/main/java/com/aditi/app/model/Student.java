ddaf68c80cf41087d43a6794f9e0dfe36c3d1fb4
aditi <aditisethia1997@gmail.com>
2024-02-05 17:23:28 +0530

    second

--- src/main/java/com/aditi/app/model/Student.java
+++ src/main/java/com/aditi/app/model/Student.java
@@ -1,5 +1,6 @@
 package com.aditi.app.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Student {
 
 	@Id
